d9c1afd07184b23b6f1d4a76419af7c6bce35426
Ali-Zafar-Grotabyte <ali.zafar@grotabyte.com>
2022-07-18 00:48:46 +0500

    added migrations for db, completed crud functionality (untested) for products

--- ServerSide/New_Architecture/src/index.ts
+++ ServerSide/New_Architecture/src/index.ts
@@ -1,7 +1,8 @@
 
-const express = require('express');
-const app = express();
 import ProductRoutes from './features/products/routes/index';
+import express from 'express';
+
+const app = express();
 
 app.use(express.json());
 app.use('/api', ProductRoutes);
