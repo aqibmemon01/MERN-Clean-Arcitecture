c23958d652e624d3cd95b5a20612e5be9e046b28
aqibmemon01 <maqibmemon786@gmail.com>
2022-11-14 19:27:36 +0500

    Fix Body Parser issues for get Request Body

--- ServerSide/New_Architecture/src/features/business/routes/index.ts
+++ ServerSide/New_Architecture/src/features/business/routes/index.ts
@@ -10,16 +10,18 @@ router.get('/business/getAll', (req: Request, res: Response) => {
 router.post('/business/create', (req: Request, res: Response) => {
     // #swagger.tags = ['Business']
     // let { name, email, password } = req.body;
+    console.log(req.body)
     BusinessController.create(req, res)
 });
 router.put('/business/update', (req: Request, res: Response) => {
     // #swagger.tags = ['Business']
-    let { name, email, password, businessId } = req.body;
+    let { name, email, password, businessId } = req.body ;
     BusinessController.update(req, res)
 });
 router.delete('/business/delete', (req: Request, res: Response) => {
     // #swagger.tags = ['Business']
     let { businessId } = req.query;
+    console.log(req.query)
     BusinessController.delete(req, res)
 });
 
